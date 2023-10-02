85b07b60bd108ccaa638b5539d2328ff394b9b34
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-02 17:08:51 +0300

    finished

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,4 @@
-# automate the task of creating a custom HTTP header response
+# Use Puppet to automate the task of creating a custom HTTP header response
 
 exec {'update':
   command => '/usr/bin/apt-get update',
