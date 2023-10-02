c463ef7eb74e60502afce885e41b457c5fa78163
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-02 17:17:11 +0300

    finished

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,10 +8,10 @@ package {'nginx':
   ensure => 'installed',
 }
 
-file_line { 'add_header':
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => "^\tlocation / {",
-  line   => "\t\tadd_header X-Served-By \"${::hostname}\";",
+file_line { 'http_header':
+  path  => /etc/nginx/sites-enabled/default,
+  after => 'listen 80 default_server',
+  line  => "add_header X-Served-By \"${::hostname}\";",
   notify => Exec['run'],
 }
 
