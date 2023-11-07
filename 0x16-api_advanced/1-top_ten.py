aa380dca0e79c7b47396045433b65a59267cc2c7
kaleabendrias <kaleabendrias1212@gmail.com>
2023-11-07 18:48:38 +0300

    reddit query

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and prints the titles of the first 10 hot"""
+import requests
+
+
+def top_ten(subreddit):
+    """function that queries the Reddit API and prints the titles of the first 10 hot"""
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json().get('data')
+    children = data.get('children')
+
+    for i in range(10):
+        print(children[i].get('data').get('title'))
