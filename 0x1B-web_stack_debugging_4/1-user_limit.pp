419fe533b7641448f3d7db7eae95c72dd456eeb3
kaleabendrias <kaleabendrias1212@gmail.com>
2023-11-16 17:14:47 +0300

    edited the file

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 # puppet file that changes file discriptors for user holberton
 exec {
     'replace the hard limit':
-    command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 1500" /etc/security/limits.conf',
+    command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 1500/" /etc/security/limits.conf',
     path    => ['/bin/','/usr/bin/']
 }
 
 exec {
     'replace the soft limit':
-    command => 'sed -i "s/holberton soft nofile 4/holberton hard nofile 1000" /etc/security/limits.conf',
+    command => 'sed -i "s/holberton soft nofile 4/holberton hard nofile 1000/" /etc/security/limits.conf',
     path    => ['/bin/','/usr/bin/']
 }
